09835fb7a2f807f15655f9072beced1ce23f0dac
kazuhiro-na <narakhope@gmail.com>
2023-09-21 08:51:22 +0900

    仮の共通ヘッダーを作成

---
+++ resources/views/layouts/admin_layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>influencer_education</title>
+</head>
+<body>
+  <header class="header-content">
+    <nav>
+        <a class="nav-button" href="/">授業管理</a>
+        @if (Auth::check())
+            <a class="nav-button" href="/">お知らせ管理</a>
+            <a class="nav-button" href="/">バナー管理</a>
+                <form method="POST" action="/logout">
+                    @csrf
+                    <button class="logout-button" type="submit">ログアウト</button>
+                </form>
+        @else
+            <a class="nav-button" href="/">ログイン</a>
+        @endif
+    </nav>
+  </header>
+  <div class="container">
+      @yield('content')
+  </div>
+</body>
+</html>
