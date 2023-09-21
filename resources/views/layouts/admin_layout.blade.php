5bbe3c9134f4257270d704ca4225298a161b20a8
kazuhiro-na <narakhope@gmail.com>
2023-09-22 07:53:00 +0900

    bootstrapのCDNを導入

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <title>influencer_education</title>
 </head>
 <body>
